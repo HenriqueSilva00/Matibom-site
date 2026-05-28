a5ccf5450743cbdd88a765ef7d0f37e251b04a4f
HenriqueSilva00 <Henrique_borges_silva@hotmail.com>
2026-05-28 11:37:26 +0100

    MainBanner fixed

--- components/MainBanner.tsx
+++ components/MainBanner.tsx
@@ -3,14 +3,14 @@ export default function MainBanner() {
     <section className="relative w-full h-screen overflow-hidden">
       {/* IMAGEM DESKTOP */}
       <img
-        src="/assets/Mainbanner.png"
+        src="/assets/MainBanner.png"
         alt="MATIBOM Banner"
         className="hidden md:block w-full h-full object-cover"
       />
 
       {/* IMAGEM MOBILE */}
       <img
-        src="/assets/Mainbanner-mobile.png"
+        src="/assets/MainBanner-mobile.png"
         alt="MATIBOM Banner Mobile"
         className="block md:hidden w-full h-full object-cover"
       />
